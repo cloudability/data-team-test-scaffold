9733d5dc34a85eb959f9b381be9e590393a455e1
Nate Putnam <nate@cloudability.com>
2018-05-18 16:33:43 -0700

    init commit

---
+++ src/main/java/com/cloudability/test/Main.java
@@ -0,0 +1,9 @@
+package com.cloudability.test;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // TODO code goes here!
+    }
+
+}
---
+++ src/test/java/com/clouability/test/tests/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.clouability.test.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void justPasses() throws Exception {
+        Assert.assertTrue(true);
+    }
+}
